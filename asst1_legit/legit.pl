17a33c014eac5e22b6bd096ae9324382611767cd
dlimit7 <daniel.li9721@gmail.com>
2018-09-29 20:16:02 +1000

    log done

--- asst1_legit/legit.pl
+++ asst1_legit/legit.pl
@@ -8,7 +8,7 @@ if (scalar @ARGV == 0)
 } elsif ($ARGV[0] eq "init") 
 {
     # Create an empty Legit repo. Error if already exists
-    mkdir ".legit" or print STDERR "legit.pl: error: .legit already exists\n" and exit;
+    mkdir ".legit" or print STDERR "legit.pl: error: .legit already exists\n" and exit 1;
     print "Initialized empty legit repository in .legit\n";
 
 } elsif ($ARGV[0] eq "add") 
@@ -20,15 +20,16 @@ if (scalar @ARGV == 0)
     
     } else 
     {
-
+        $#ARGV > 0 or print STDERR "usage: legit.pl add <filenames>\n" and exit 1;
         # create index if it doesn't already exist
         -d ".legit/index" or mkdir ".legit/index";
         # Check if they are "ordinary files" that also exist.
         foreach my $i (1..$#ARGV) {
             print "$ARGV[$i]\n\n";
-            $ARGV[$i] =~ m/^[a-zA-z0-9\.\-\_]+$/g or print STDERR "legit.pl: error: invalid filename '$ARGV[$i]'\n" and exit;
+            $ARGV[$i] =~ m/\-\-\-/g and print STDERR "usage: legit.pl add <filenames>\n" and exit 1;
+            $ARGV[$i] =~ m/^[a-zA-Z0-9][a-zA-z0-9\.\-\_]*$/g or print STDERR "legit.pl: error: invalid filename '$ARGV[$i]'\n" and exit 1;
             if (not -e $ARGV[$i]) {
-                print "legit.pl: error: can not open '$ARGV[$i]'\n" and exit;
+                print "legit.pl: error: can not open '$ARGV[$i]'\n" and exit 1;
             }
         }
         print "adding files now\n";
@@ -62,26 +63,31 @@ if (scalar @ARGV == 0)
         $commit_no++;
         $commit_dir = ".legit/commit.$commit_no";
     }
-    $commit_message = $ARGV[$i];
+    $commit_message = $ARGV[$i+1];
     print "Commit message is $commit_message\n";
     # Save a copy of all files in the index to the repo or print "nothing to commit" if index hasn't changed compared to prev commit
     if ($commit_no > 0) 
     {
         $commit = 0;
         $prev_commit_no = $commit_no-1;
-        print "$prev_commit_no\n";
+        print "prev commit no $prev_commit_no\n";
         foreach $index_file (glob ".legit/index/*") {
             print "Checking";
             $index_file =~ m/^\.legit\/index\/(.+)$/g;
             $file = $1;
             printf "file is $file\n";
-
-            if (compare_files("$index_file", ".legit/commit.$prev_commit_no/$file")) {
+            if (not -e ".legit/commit.$prev_commit_no/$file") {
+                $commit = 1;
+            } elsif (compare_files("$index_file", ".legit/commit.$prev_commit_no/$file")) {
                 print "Files are different\n";
                 $commit = 1;
                 last;
             }
         }
+        open $log, ">>", ".legit/log.txt" or die;
+    } else {
+        print "Creating log.txt\n";
+        open $log, ">", ".legit/log.txt" or die;
     }
     if ( $commit ) {
         printf("its time to COMMIT\n");
@@ -99,24 +105,31 @@ if (scalar @ARGV == 0)
                 $line = shift @arr;
                 print F "$line";
             }
+            close F;
             $commit=0;
         }
+        print $log "$commit_no $commit_message\n";
     } else {
         print STDERR "Nothing to commit\n";
     }
-    
+    close $log;
 
 } elsif ($ARGV[0] eq "log") 
 {
-    $commit_no == -1 and print STDERR "legit.pl: error: your repository does not have any commits yet\n";
+    -d ".legit/commit.0" or print STDERR "legit.pl: error: your repository does not have any commits yet\n";
     # use log.txt in .legit
+    open F, "<", ".legit/log.txt" or die;
+    @lines = reverse <F>;
+    foreach $line (@lines) {
+        print "$line";
+    }
 
 } elsif ($ARGV[0] eq "show") 
 {
     # first check if any commits exist --> enough to check if .legit/commit.0 exists
-    -d ".legit/commit.0" or print STDERR "legit.pl: error: your repository does not have any commits yet\n" and exit;
+    -d ".legit/commit.0" or print STDERR "legit.pl: error: your repository does not have any commits yet\n" and exit 1;
     # print file contents from commit or index
-    ($#ARGV > 0) or print STDERR "usage: legit.pl show <commit>:<filename>\n" and exit;
+    ($#ARGV > 0) or print STDERR "usage: legit.pl show <commit>:<filename>\n" and exit 1;
     if ( $ARGV[1] =~ m/^(\d+):(.+)$/g ) 
     {
         $commit = $1;
@@ -128,13 +141,13 @@ if (scalar @ARGV == 0)
         $path = ".legit/index";
         $filename = $1;
     } else {
-        print STDERR "legit.pl: error: invalid filename''" and exit;
+        print STDERR "legit.pl: error: invalid filename''" and exit 1;
     }
     # Check if path exists
-    -d $path or print STDERR "legit.pl: error: unknown commit '$commit'\n" and exit;
+    -d $path or print STDERR "legit.pl: error: unknown commit '$commit'\n" and exit 1;
     $path = "$path/$filename";
     # Check if the file actually exists
-    -e $path or print STDERR "legit.pl: error: '$filename' not found in commit $commit\n" and exit;
+    -e $path or print STDERR "legit.pl: error: '$filename' not found in commit $commit\n" and exit 1;
     # Print the file to stdout
     open F, "<", "$path" or die;
     while ( $line = <F> ) {
@@ -146,7 +159,7 @@ if (scalar @ARGV == 0)
 } else 
 {
     print STDERR "legit.pl: error: unknown command $ARGV[0]\n";
-    print_usage() and exit;
+    print_usage() and exit 1;
 }
 
 
